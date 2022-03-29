d4615459493915e50dbff1e337a87fe02d6c5407
amberopsteen <94449057+amberopsteen@users.noreply.github.com>
2022-03-29 13:47:05 +0200

    update menu

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" type="media/favicon" href="./media/logo_stone.jpg" />
+    <link rel="icon" type="media/favicon" href="./media/grill_logo.jpg" />
     <title>Grillmasters</title>
     <link
       rel="stylesheet"
--- menu.php
+++ menu.php
@@ -6,7 +6,7 @@
       $result = $stmt->fetchAll();
 
       foreach ($result as $object) {
-        echo $object['naam'];
+        echo $object['omschrijving'];
       }
     ?>
     <div class="titel_menu">
@@ -23,7 +23,7 @@
     </div>
     <div class="menu">
       <div class="dish">
-        <div class="images">
+        <div class="image">
           <img id="tournedos" src="./media/menu_5.jpg" />
         </div>
         <div class="product">
@@ -32,26 +32,26 @@
           <p class="desc">
             Tournedos mousseline | ratatouille | paddenstoelen | jus
           </p>
-          <div class="buttons">
-            <button class="add">Toevoegen</button>
+          <div class="button">
+            <button>Toevoegen</button>
           </div>
         </div>
       </div>
       <div class="dish">
-        <div class="images">
+        <div class="image">
           <img src="./media/menu_6.jpg" />
         </div>
         <div class="product">
           <h2>Grilled pork belly</h2>
           <h3>20,-</h3>
           <p class="desc">meest malste stuk van de rund | kruidencrème</p>
-          <div class="buttons">
-            <button class="add">Toevoegen</button>
+          <div class="button">
+            <button>Toevoegen</button>
           </div>
         </div>
       </div>
       <div class="dish">
-        <div class="images">
+        <div class="image">
           <img src="./media/menu_3.jpg" />
         </div>
         <div class="product">
@@ -60,25 +60,23 @@
           <p class="desc">
             zalmfilet│ rozemarijn│ honing│ crème van mierikswortel
           </p>
-          <div class="buttons">
-            <button class="add">Toevoegen</button>
+          <div class="button">
+            <button>Toevoegen</button>
           </div>
         </div>
       </div>
       <div class="dish">
-        <div class="images">
+        <div class="image">
           <img src="./media/Afbeelding7.jpg" />
         </div>
         <div class="product">
           <h2>Grilled steak</h2>
           <h3>30,-</h3>
           <p class="desc">
-            200 gram, met saus naar keuze:
-            stroganoffsaus/pepersaus/champignonsaus of gebakken uien en
-            champignons.
+          meest malste stuk van de rund | kruidencrème
           </p>
-          <div class="buttons">
-            <button class="add">Toevoegen</button>
+          <div class="button">
+            <button>Toevoegen</button>
           </div>
         </div>
       </div>
@@ -88,41 +86,41 @@
     </div>
     <div class="menu">
       <div class="dish">
-        <div class="images">
+        <div class="image">
           <img id="tournedos" src="./media/Afbeelding6.jpg" />
         </div>
         <div class="product">
           <h2>Grilled haddock</h2>
           <h3>25,-</h3>
           <p class="desc">Grilled haddock | tomatoes | lemon | dill</p>
-          <div class="buttons">
-            <button class="add">Toevoegen</button>
+          <div class="button">
+            <button>Toevoegen</button>
           </div>
         </div>
       </div>
       <div class="dish">
-        <div class="images">
+        <div class="image">
           <img src="./media/Afbeelding5.jpg"/>
         </div>
         <div class="product">
           <h2>Grilled tilapia</h2>
           <h3>20,-</h3>
           <p class="desc">Grilled tilapia | pearl couscous | lemon | thyme</p>
-          <div class="buttons">
-            <button class="add">Toevoegen</button>
+          <div class="button">
+            <button>Toevoegen</button>
           </div>
         </div>
       </div>
       <div class="dish">
-        <div class="images">
+        <div class="image">
           <img src="./media/Afbeelding4.jpg" />
         </div>
         <div class="product">
           <h2>Grilled eel</h2>
           <h3>25,-</h3>
           <p class="desc">Grilled eel | spring union | lemon | thyme</p>
-          <div class="buttons">
-            <button class="add">Toevoegen</button>
+          <div class="button">
+            <button>Toevoegen</button>
           </div>
         </div>
       </div>
@@ -132,28 +130,28 @@
     </div>
     <div class="menu">
       <div class="dish">
-        <div class="images">
+        <div class="image">
           <img id="tournedos" src="./media/Afbeelding2.jpg" />
         </div>
         <div class="product">
           <h2>Grilled beetroot</h2>
           <h3>18,-</h3>
           <p class="desc">Grilled beetroot | basil | garlic | olive oil</p>
-          <div class="buttons">
-            <button class="add">Toevoegen</button>
+          <div class="button">
+            <button>Toevoegen</button>
           </div>
         </div>
       </div>
         <div class="dish">
-          <div class="images">
+          <div class="image">
             <img src="./media/Afbeelding3.jpg" />
           </div>
           <div class="product">
             <h2>Grilled vegetarian meatballs</h2>
             <h3>20,-</h3>
             <p class="desc">Grilled vegetables | vegetarian meatballs | herbs | olive oil</p>
-            <div class="buttons">
-              <button class="add">Toevoegen</button>
+            <div class="button">
+              <button>Toevoegen</button>
             </div>
           </div>
         </div>
