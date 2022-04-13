8adcfc5f1c868bd5f6f3ff86b911df9ea6dd61d3
amberopsteen <94449057+amberopsteen@users.noreply.github.com>
2022-04-13 10:48:02 +0200

    update test

--- shoppingcart.php
+++ shoppingcart.php
@@ -3,7 +3,6 @@
     ?>
     <div id="shoppingcart" class="shopping_cart">
       <h2>Shopping cart</h2>
-      <p>your shopping cart is empty</p>
     </div>
     <?php
       include("includes/footer.php")
